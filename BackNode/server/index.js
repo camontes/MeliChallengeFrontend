f0bc69ab80e3f5fe9ddd3c9f08662b1ee9111f44
camontes <pope624@hotmail.com>
2023-02-10 17:11:43 -0500

    enable cors * and conditional urls

--- BackNode/server/index.js
+++ BackNode/server/index.js
@@ -99,7 +99,7 @@ const app = express();
 
   app.get('/api/items/:id', async(req, res) => {
 
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.set('Access-Control-Allow-Origin', '*');
 
     const {id} = req.params;
 
@@ -160,7 +160,7 @@ const app = express();
 
  app.get('/api/items', async  function(req, res){
 
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.set('Access-Control-Allow-Origin', '*');
 
   let data = {
     categories: [],
--- src/api/meli.js
+++ src/api/meli.js
@@ -5,5 +5,5 @@ const prodUrl = "https://melifront.fly.dev/"
 
 export default axios.create({
 
-    baseURL: prodUrl
+    baseURL: process.env.NODE_ENV !== 'production' ? devUrl : prodUrl
 });
