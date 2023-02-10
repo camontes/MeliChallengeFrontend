50200adf981f708de635c1122d7a142256f34814
camontes <pope624@hotmail.com>
2023-02-10 16:39:22 -0500

    Add Url api production

--- src/api/meli.js
+++ src/api/meli.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
+const devUrl = "http://localhost:3001/";
+const prodUrl = "https://melifront.fly.dev/"
+
 export default axios.create({
 
-    baseURL: 'http://localhost:3001/'
+    baseURL: prodUrl
 });
